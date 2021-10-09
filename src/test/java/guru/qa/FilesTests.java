756c35dce1c4dda320a192dedec4863ec8ca47ec
mgorbatenkov <mgorgatenkov@yahoo.com>
2021-10-09 15:38:37 +0300

    added pdf test

--- src/test/java/guru/qa/FilesTests.java
+++ src/test/java/guru/qa/FilesTests.java
@@ -1,5 +1,6 @@
 package guru.qa;
 
+import com.codeborne.pdftest.PDF;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -8,11 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilesTests {
     @Test
-    void txtTest() throws Exception{
+    void txtTest() throws Exception {
         String result;
-        try(InputStream is = getClass().getClassLoader().getResourceAsStream("files/sample.txt")){
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("files/sample.txt")) {
             result = new String(is.readAllBytes(), "UTF-8");
         }
         assertThat(result).contains("memoria torquentur");
+    }
+
+    @Test
+    void pdfTest() throws Exception {
+        PDF parsed = new PDF(getClass().getClassLoader().getResource("files/sample.pdf"));
+        assertThat(parsed.numberOfPages).isGreaterThan(3);
+        System.out.println();
     }
 }
