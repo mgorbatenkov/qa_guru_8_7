feb62f1c801c887f6cf0c338874ad66b8853af1b
mgorbatenkov <mgorgatenkov@yahoo.com>
2021-10-09 18:18:59 +0300

    added doc test

--- src/test/java/guru/qa/FilesTests.java
+++ src/test/java/guru/qa/FilesTests.java
@@ -3,12 +3,12 @@ package guru.qa;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.model.FileHeader;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,11 +42,18 @@ public class FilesTests {
     @Test
     void zipWithPasswordTest() throws Exception {
         ZipFile zipFile = new ZipFile("src/test/resources/files/sample.zip", "qwerty".toCharArray());
-        try (InputStream is = zipFile.getInputStream(zipFile.getFileHeader("test.txt")))
-        {
+        try (InputStream is = zipFile.getInputStream(zipFile.getFileHeader("test.txt"))) {
             String result = new String(is.readAllBytes(), "UTF-8");
             assertThat(result).contains("Testing");
         }
     }
 
+    @Test
+    void docTest() throws Exception {
+        try (FileInputStream fis = new FileInputStream("src/test/resources/files/sample.docx")) {
+            XWPFDocument document = new XWPFDocument(fis);
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+            assertThat(extractor.getText()).contains("velit vitae libero");
+        }
+    }
 }
